49f2493ada9572959fa2676ad0a92fa3def716f4
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-06 21:06:21 +0100

    Conexion a base de datos, gestor de errores mas validacion de de datos de apis, junto a CR del CRUD

---
+++ src/controllers/ProjectController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express"
+import Project from "../models/Project"
+
+
+export class ProjectController{
+    
+    static createProject = async (req: Request, res: Response) => {
+        const project = new Project(req.body)
+        try {
+            await project.save() 
+            res.send('Proyecto creado correctamente')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    static getAllProjects = async (req: Request, res: Response) => {
+        
+        try {
+            const projects = await Project.find({})
+            res.send(projects)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    static getProjectById = async (req: Request, res: Response) => {
+        const {id} = req.params
+        try {
+            const project = await Project.findById(id)
+            if(!project){
+                const errorr = new Error('Proyecto no encontrado')
+                return res.status(404).json({error: errorr.message})
+            }
+            res.send(project)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db'
+import projectRoutes  from './routes/projectRoutes'
+
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
+//Habilitando el modelo de JSON para los bodys
+app.use(express.json())
+
+//Routes
+app.use('/api/projects', projectRoutes)
 
 export default app
