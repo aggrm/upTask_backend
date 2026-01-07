f1ffe6c65c025058a5dcdfe15182fe668e2f8df5
Alberto Goujon <alberto.goujon@gmail.com>
2026-01-07 03:18:53 +0100

    creacion del modelo task y referenciarlo en proyecto

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,9 +1,11 @@
-import mongoose, { Schema, Document} from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc, Types} from "mongoose";
+import { ITask } from "./Task";
 
-export type ProjectType = Document & {
+export interface IProject extends Document {
     projectName: string
     clientName: string
     description: string
+    task: PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema: Schema = new Schema({
@@ -21,8 +23,14 @@ const ProjectSchema: Schema = new Schema({
         type: String,
         require: true,
         trim: true
-    }
-})
+    },
+    task:[
+        {
+            type: Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+}, {timestamps: true})
 
-const Project = mongoose.model<ProjectType>('Project', ProjectSchema)
+const Project = mongoose.model<IProject>('Project', ProjectSchema)
 export default Project
